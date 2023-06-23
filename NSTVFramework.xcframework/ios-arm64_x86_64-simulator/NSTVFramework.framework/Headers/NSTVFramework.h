2337e3cd4d2077ed12ae98a680d7b5eb63bb8721
Sotheavuth Nguon <sotheavuth.nguon@yahoo.com>
2023-06-23 13:46:56 +0700

    Added XCFramework 1.0.0

---
+++ NSTVFramework.xcframework/ios-arm64_x86_64-simulator/NSTVFramework.framework/Headers/NSTVFramework.h
@@ -0,0 +1,18 @@
+//
+//  NSTVFramework.h
+//  NSTVFramework
+//
+//  Created by Sotheavuth.Nguon on 23/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NSTVFramework.
+FOUNDATION_EXPORT double NSTVFrameworkVersionNumber;
+
+//! Project version string for NSTVFramework.
+FOUNDATION_EXPORT const unsigned char NSTVFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSTVFramework/PublicHeader.h>
+
+
